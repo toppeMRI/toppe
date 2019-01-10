f57433eed8326f9b6b62db2d1f85a166a02ac4a3
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2019-01-10 15:03:21 -0500

    wip On branch spgr Your branch is up to date with 'origin/spgr'.
    Changes to be committed:
    modified:   main.m

--- examples/SPGR/main.m
+++ examples/SPGR/main.m
@@ -17,14 +17,8 @@ fov  = [24 24 10];       % cm
 flip = 10;               % excitation flip angle (degrees)
 ncyclesspoil = 2;        % number of cycles of spoiler phase across voxel dimension (applied along x and z)
 
-if fov(1) ~= fov(2)
-	error('In-plane field-of-view must be square');
-end
-
-vox = fov./matrix;      % voxel dimensions (cm).
-
-if vox(1) ~= vox(2)
-	error('In-plane voxel size must be square');
+if matrix(1) ~= matrix(2) | fov(1) ~= fov(2)
+	error('In-plane fov and voxel size must be square');
 end
 
 %% Make .mod file containing z spoiler gradient and RF excitation
@@ -44,6 +38,44 @@ toppe.utils.makegre(fov(1), matrix(1), fov(3)/matrix(3), ...
 %toppe.plotmod(ofname);
 
 %% Create scanloop.txt
+rfmodnum = 1;           % module index, i.e., line number in modules.txt
+readoutmodnum = 2;
+rfphs = 0;              % radians
+rf_spoil_seed_cnt = 0;
+rf_spoil_seed = 117;      % degrees
+for iz = 0:matrix(3)    % We'll use iz=0 for approach to steady-state
+	for iy = 1:matrix(2)
+		% rf excitation
+		loop{ii}.module = rfmod;   
+		loop{ii}.flip = 10;        % can't exceed design flip angle ('flip')
+		loop{ii}.gxscale = 0;
+		loop{ii}.gyscale = 0;
+		loop{ii}.gzscale = 1;      % full amplitude (range is [-1 1])
+		rfphs = rfphs + (rf_spoil_seed/180 * pi)*rf_spoil_seed_cnt;  % radians
+		loop{ii}.rfphs == angle(exp(1i*(rfphs)));
+
+      ia_gz = 2*round( max_pg_iamp*(((iz-1+0.5)-nz/2)/(nz/2)) /2);   % z phase-encode amplitude
+      else   % discarded acquisitions (disdaqs)
+         ia_gz = 0;
+      end
+      dabslice = max(iz, 0);
+
+      for il = 1:nl
+         dabview = il;    % skip baseline (0) view as usual
+         phi = round(2*pi*(il-1)/nl/pi*max_pg_iamp);
+
+         % phase for rf spoiling
+         rfphase = rfphase + (rf_spoil_seed/180 * pi)*rf_spoil_seed_cnt;  % radians
+         rf_spoil_seed_cnt = rf_spoil_seed_cnt + 1;
+         rfphasetmp = atan2(sin(rfphase), cos(rfphase));      % wrap phase to (-pi,pi) range
+         irfphase = 2*round(rfphasetmp/pi*max_pg_iamp/2);     % short int 
+
+	end
+end
+
+
+
+scanloop
 
 return;
 
