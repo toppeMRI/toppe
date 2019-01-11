4aa64671384db33786bf51b3a7fb254621941e12
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2019-01-11 13:11:22 -0500

    On branch spgr Changes to be committed: modified:   main.m new file:   modules.txt deleted:    writeloop.m

--- examples/SPGR/main.m
+++ examples/SPGR/main.m
@@ -1,9 +1,5 @@
+function main
 % 3D SPGR sequence example
-
-%import toppe.*
-%import toppe.utils.*
-%import toppe.utils.rf.*
-%import toppe.utils.rf.jpauly.*
 
 %% Set system limits
 % Good to back off a bit on slew to avoid PNS (even if scanner can slew at higher rate).
@@ -12,7 +8,7 @@ sys = toppe.systemspecs('maxSlew', 10, 'maxGrad', 4);
 
 %% Acquisition parameters
 % fov and voxel size must be square (in-plane)
-matrix = [240 240 50];
+matrix = [24 24 10];
 fov  = [24 24 10];       % cm
 flip = 10;               % excitation flip angle (degrees)
 ncyclesspoil = 2;        % number of cycles of spoiler phase across voxel dimension (applied along x and z)
@@ -43,7 +39,7 @@ readoutmod = 2;
 rfphs = 0;              % radians
 rf_spoil_seed_cnt = 0;
 rf_spoil_seed = 117;    % degrees
-ii = 0;                 % counts number of module executions
+ii = 1;                 % counts number of module executions
 ny = matrix(2);
 nz = matrix(3);
 
@@ -51,50 +47,96 @@ dabmode = dabon;   % best to leave acquisition on even during disdaqs so auto-pr
 waveform = 1;
 textra = 0;        % add delay at end of module (int, microseconds)
 
+loop = containers.Map;
+
 for iz = 0:nz           % We'll use iz=0 for approach to steady-state
+iz
 	for iy = 1:ny
 
 		% rf excitation
-		ii = ii + 1;
-		module = rfmod;   
-		rfscale = 1.0;               % full amplitude (range is [-1 1]) 
-		gxscale = 0;
-		gyscale = 0;
-		gzscale = 1.0;      %
-		rfphs = rfphs + (rf_spoil_seed/180 * pi)*rf_spoil_seed_cnt;  % radians
+		block = {'module', rfmod, ...
+			'rfscale', 1.0, ...           % full amplitude (range is [-1 1]) 
+			'gxscale', 0, ...
+			'gyscale', 0, ...
+			'gzscale', 1.0, ...
+			'rfphs',   rfphs + (rf_spoil_seed/180 * pi)*rf_spoil_seed_cnt };  % radians
 		rf_spoil_seed_cnt = rf_spoil_seed_cnt + 1;
-		d(icnt,:) = [tipdowncore ia_tipdown ia_th max_pg_iamp max_pg_iamp max_pg_iamp dabslice dabecho dabview daboff 0 irfphase irfphase textra rffreq waveform]; 
+
+		d(ii,:) = sub_block2vec(block);
+		ii = ii + 1;
 
 		% readout
+		block = {'module', readoutmod, ...
+			'gxscale', 1.0, ...
+			'gyscale', ((iy-1+0.5)-ny/2)/(ny/2), ...   % phase-encode amplitude scaling
+			'gzscale', ((iz-1+0.5)-nz/2)/(nz/2), ...   % partition-encode amplitude scaling
+			'dabslice', max(iz,0), ...                 % Convention: skip dabslice=0 
+			'dabecho',  0, ...    
+			'dabview', iy, ...                         % Convention: skip baseline (0) view
+			'recphs', loop{ii}.rfphs};
+		d(ii,:) = sub_block2vec(block);
 		ii = ii + 1;
-		loop{ii}.module = readoutmod;   
-		loop{ii}.gxscale = 1.0;
-		loop{ii}.gyscale = ((iy-1+0.5)-ny/2)/(ny/2);   % phase-encode amplitude scaling
-		if iz > 0
-			loop{ii}.gzscale = ((iz-1+0.5)-nz/2)/(nz/2);   % partition-encode amplitude scaling
-			loop{ii}.dabslice = max(iz,0);    % Convention: skip dabslice=0 
-			loop{ii}.dabecho = 0;
-			loop{ii}.dabview = iy;            % Convention: skip baseline (0) view
-		else
-			loop{ii}.gzscale = 0;    % approach to steady-state
-			loop{ii}.dabslice = 0;
-			loop{ii}.dabecho = 0;
-			loop{ii}.dabview = 0;
-		end
-		loop{ii}.recphs = loop{ii}.rfphs;
 	end
 end
 
-toppe.loopstruct2txt(loop);
+% write loop array to scanloop.txt
+toppe.loop2txt(d);
+
+%% Play sequence in loop (movie) mode
+%system('rm scan,spgr.tgz');
+%system('tar czf scan,spgr.tgz modules.txt scanloop.txt *.mod *.m ');
+%figure; toppe.plotseq(5000,5010); subplot(511); title('SPGR');
+%playseq(2,0);
 
 return;
 
-writeloop;                     % create scanloop.txt
+% Convert paired cell array to a line entry in scanloop.txt
+function d = sub_block2vec(block)
+% Input:
+%  block     {'field 1', value1, 'field2', value2, ...} paired cell array containing TOPPE settings for the execution of one module
+% Output:
+% d          [1 16] vector containing values to be written to scanloop.txt
+%            
+%            [tipdowncore ia_tipdown ia_th max_pg_iamp max_pg_iamp max_pg_iamp dabslice dabecho dabview daboff rot irfphase irfphase textra rffreq waveform]; 
 
-system('rm scan,spgr.tgz');
-system('tar czf scan,spgr.tgz modules.txt scanloop.txt *.mod *.m ');
+max_pg_iamp = 2^15-2;    % max amplitude in hardware units
 
-figure; toppe.plotseq(5000,5010); subplot(511); title('SPGR');
-%playseq('scanloop.txt',3,0);
+% initialize
+d = zeros(1,16);
+
+% replace with those values that are provided
+%try
+	d(1) = block.module;
+%catch
+%	error('module number must be specified');
+%end
+if isfield(block, 'rfscale')
+	d(2) = 2*round(max_pg_iamp * block.rfscale/2);   % force even amp
+end
+if isfield(block, 'thscale')
+	d(3) = 2*round(max_pg_iamp * block.thscale/2);
+else
+	d(3) = max_pg_iamp;
+end
+if isfield(block, 'gxscale')
+	d(4) = 2*round(max_pg_iamp * block.gxscale/2);
+end
+if isfield(block, 'gyscale')
+	d(5) = 2*round(max_pg_iamp * block.gxscale/2);
+end
+if isfield(block, 'gzscale')
+	d(6) = 2*round(max_pg_iamp * block.gxscale/2);
+end
+if isfield(block, 'dabslice')
+	d(7) = block.dabslice;
+end
+if isfield(block, 'dabecho')
+	d(8) = block.dabecho;
+end
+if isfield(block, 'dabview')
+	d(9) = block.dabview;
+end
+
+return;
 
 
