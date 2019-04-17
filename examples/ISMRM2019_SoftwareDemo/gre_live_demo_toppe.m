88a3d6858a0dbdd3697fdc2872781abdeb784b78
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2019-04-17 17:31:24 -0400

    Changes to be committed: modified:   gre_live_demo_toppe.m

--- examples/ISMRM2019_SoftwareDemo/gre_live_demo_toppe.m
+++ examples/ISMRM2019_SoftwareDemo/gre_live_demo_toppe.m
@@ -1,3 +1,6 @@
+% To use the +toppe Matlab package, include the (root) folder containing the +toppe folder in your Matlab path.
+addpath ~/gitlab/toppe/
+
 % step
 % 0 ... Basic sequence
 % 1 ... Add spoiler in read, phase and slice (vary spoiler?)
@@ -5,8 +8,6 @@
 % 3 ... Vary RF phase quasi-randomly
 % 4 ... Make receiver phase follow transmitter phase
 step = 4;
-
-% To use the +toppe Matlab package, include the (root) folder containing the +toppe folder in your Matlab path.
 
 % Define FOV and resolution
 n = 128;
@@ -16,9 +17,7 @@ sliceThickness = 0.5;    % cm
 fov  = 25.6;             % cm
 
 % Define sequence parameters
-alpha = 30;              % excitation angle (degrees)
-TE = 10;                 % msec
-TR = 20;
+alpha = 20;              % excitation angle (degrees)
 ncyclesspoil = 2;        % number of cycles of spoiler phase across voxel dimension (applied along x)
 
 % set system limits
@@ -27,7 +26,7 @@ sys = toppe.systemspecs('maxSlew', 130, 'slewUnit', 'T/m/s', 'maxGrad', 25, 'gra
 % Create slice selection pulse and gradient ('tipdown.mod')
 ofname = 'tipdown.mod';     % Output file name
 dur = 2;                    % RF pulse duration (msec)
-ftype = 'min';              % minimum-phase SLR pulse (good for 3D imaging)
+ftype = 'ls';
 tbw = 8;                    % time-bandwidth product of SLR pulse 
 toppe.utils.rf.makeslr(alpha, sliceThickness, tbw, dur, ncyclesspoil, ...
                        'ftype', ftype, 'ofname', ofname, 'system', sys);
@@ -46,54 +45,28 @@ rf_spoil_seed_cnt = 0;
 rf_spoil_seed = 117;    % degrees
 ii = 1;                 % counts number of module executions
 
-dabon = 1;
-daboff = 0;
-dabmode = dabon;
-waveform = 1;
-textra = 0;        % add delay at end of module (int, microseconds)
-
-write2loop('setup');
+toppe.write2loop('setup');
 for iy = -10:n   % We'll use iy<1 for approach to steady-state
 
-	% rf excitation block (usage of 'block' here parallels its usage in Pulseq)
-	write2loop('tipdown.mod', 'RFphase', rfphs);
-	write2loop('readout.mod', 'AcqPhase', rfphs, 'view', min(iy,1));
-		block = [];
-		block.module = rfmod;
-		block.rfscale = 1.0;
-		block.gxscale = 0;
-		block.gyscale = 0;
-		block.gzscale = 1.0;
-		block.rfphs = angle(exp(1i*rfphs));   % radians
-		d(ii,:) = toppe.blockstruct2vec(block);
-		ii = ii + 1;
+	% rf excitation 
+	toppe.write2loop('tipdown.mod', 'RFphase', rfphs, 'Gamplitude', [0 0 1]');
 
-		% readout
-		block = [];
-		block.module =  readoutmod;
-		block.rfscale = 1.0;
-		block.gxscale = 1.0;
-		block.gyscale = ((iy-1+0.5)-ny/2)/(ny/2);    % phase-encode amplitude scaling, range is [-1 1]
-		block.gzscale = ((iz-1+0.5)-nz/2)/(nz/2);    % partition-encode amplitude scaling
-		block.dabslice = max(iz,0);                  % Convention: skip dabslice=0 
-		block.dabecho = 0; 
-		block.dabview = iy;                          % Convention: skip baseline (0) view
-		block.recphs = angle(exp(1i*rfphs));         % radians
-		d(ii,:) = toppe.blockstruct2vec(block);
-		ii = ii + 1;
+	% readout. Data is stored in 'slice', 'echo', and 'view' indeces.
+	daqphs = rfphs;
+	yamp = max( ((iy-1+0.5)-n/2)/(n/2), -1);    % phase-encode amplitude scaling, range is [-1 1]
+	toppe.write2loop('readout.mod', 'DAQphase', daqphs, 'view', max(iy,1), ...
+		'Gamplitude', [1 yamp 0]', 'textra', 20); 
 
-		% update rf/rec phase
-		rfphs = rfphs + (rf_spoil_seed/180 * pi)*rf_spoil_seed_cnt ;  % radians
-		rf_spoil_seed_cnt = rf_spoil_seed_cnt + 1;
-	end
+	% update rf/rec phase (RF spoiling)
+	rfphs = rfphs + (rf_spoil_seed/180 * pi)*rf_spoil_seed_cnt ;  % radians
+	rf_spoil_seed_cnt = rf_spoil_seed_cnt + 1;
 end
+toppe.write2loop('finish');
 
-% write loop array to scanloop.txt
-toppe.loop2txt(d);
 
 %% Play sequence in loop (movie) mode
 nModulesPerTR = 2;
-toppe.playseq(nModulesPerTR, 'nTRskip', 10);
+toppe.playseq(nModulesPerTR, 'nTRskip', 2);
 
 return;
 
