6cec95927e14659d24554aace8f3f1d15aa90691
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2019-04-23 13:10:58 -0400

    On branch ismrm2019 Your branch is up to date with 'origin/ismrm2019'.
    Changes to be committed:
    modified:   epi_recon.m

--- examples/ISMRM2019_SoftwareDemo/epi_recon.m
+++ examples/ISMRM2019_SoftwareDemo/epi_recon.m
@@ -14,7 +14,7 @@ if ~exist('readoutfile','var')
 	readoutfile = 'readout.mod';
 end
 
-% get readout file header
+%% get readout file header
 [~,gx,~,~,~,hdrints] = toppe.readmod(readoutfile);
 ndat = size(gx,1);
 N    = hdrints(1);       % image size
@@ -22,21 +22,21 @@ nes  = hdrints(3);       % echo spacing (number of 4us samples)
 npre = hdrints(4);       % number of samples before start of readout plateau of first echo
 nshots = size(gx,2);
 
-% load raw data
+%% load raw data
 d = toppe.utils.loadpfile(pfile); %, 1, 2, 2);               % int16, size [ndat ncoils nslices nechoes nviews] = [ndat ncoils 1 1 nshots]
 d = permute(d,[1 5 2 3 4]);         % [ndat nshots ncoils].
 d = double(d);
 d = flipdim(d,1);        % data is stored in reverse order (for some reason)
 [ndat nshots ncoils] = size(d);
 
-% apply gradient/acquisition delay
-d = circshift(d, 0);
+%% apply gradient/acquisition delay
+%d = circshift(d, 1);
 %dup = interpft(d, 5*ndat
 %for ii = 1:nshots
 %	for ic = 1:ncoils
 %		dtmp = 
 
-% sort data into 2D NxN matrix
+%% sort data into 2D NxN matrix
 d2d = zeros(N,N,ncoils);
 etl = N/nshots;    % echo-train length
 cnt = 1;
@@ -45,18 +45,27 @@ for ic = 1:ncoils
 		for jj = 1:etl
 			istart = npre + (jj-1)*nes + 1;
 			dtmp = d(istart:(istart+N-1), ii, ic);  % one echo
-			if mod(jj-1,2)
-				dtmp = flipdim(dtmp,1);              % flip every other echo within each shot
+
+			% flip echoes as needed 
+			if mod(ii,2)
+				if mod(jj-1,2)
+					dtmp = flipdim(dtmp,1);           
+				end
+			else
+				if mod(jj,2)
+					dtmp = flipdim(dtmp,1);
+				end
 			end
 			iy = (jj-1)*nshots + ii;
 			IY(cnt) = iy;
 			cnt = cnt + 1;
+
 			d2d(:,iy,ic) = dtmp;
 		end
 	end
 end
 
-% do IFT and display
+%% do IFT and display
 for ic = 1:ncoils
 	ims(:,:,ic) = fftshift(ifftn(fftshift(d2d(:,:,ic))));
 end
