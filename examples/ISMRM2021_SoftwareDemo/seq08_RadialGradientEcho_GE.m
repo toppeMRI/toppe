6acda653bc4ff7917409a304456e902632763216
jon <jfnielse@umich.edu>
2021-04-30 16:31:16 -0400

    On branch develop 	deleted:    seq08_RadialGradientEcho_GE.m

--- examples/ISMRM2021_SoftwareDemo/seq08_RadialGradientEcho_GE.m
+++
@@ -1,6 +0,0 @@
-% setup
-seq08_RadialGradientEcho;             % create 'gre_rad.seq'
-pulsegeq.seq2ge('gre_rad.seq');       % create TOPPE files (modules.txt, scanloop.txt, *.mod files)
-nModulesPerTR = 4;                    % by inspection
-toppe.playseq(nModulesPerTR);         % preview sequence (loop)
-
