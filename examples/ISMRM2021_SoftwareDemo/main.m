d46110aaccca677576cf5ad24ef6684757f8ac11
jon <jfnielse@umich.edu>
2021-04-30 16:38:31 -0400

    On branch develop 	modified:   main.m

--- examples/ISMRM2021_SoftwareDemo/main.m
+++ examples/ISMRM2021_SoftwareDemo/main.m
@@ -1,30 +1,50 @@
 %setup;
 
 seq01_Fid;
+pulsegeq.seq2ge('fid.seq');
 % rename:
 % module1.mod => tipdown.mod
 % module2.mod => readout.mod
 % Update modules.txt accordingly
 
 seq02_Fid_multipleFAs;
+pulsegeq.seq2ge('fid.seq');
 % rename:
 % module1.mod => tipdown.mod
 % module2.mod => readout.mod
 % Update modules.txt accordingly
 
 seq03_SE;
+seq03a_SE_withBackGrad;
+pulsegeq.seq2ge('se.seq');
 % rename:
 % module1.mod => tipdown.mod
 % module2.mod => readout.mod
 % Update modules.txt accordingly
 
-%seq05_RadialSE;
+seq04_SE_withSpolers;
+pulsegeq.seq2ge('se.seq');
+% rename:
+% module1.mod => tipdown.mod
+% module2.mod => readout.mod
+% Update modules.txt accordingly
+
+seq05_RadialSE;
 pulsegeq.seq2ge('se_radial.seq');
 % rename:
 % module1.mod => tipdown.mod
 % module6.mod => readout.mod
 % Update modules.txt accordingly
 toppe.playseq(4,'drawpause',false);
+
+seq06_gre_live_demo_step0;
+pulsegeq.seq2ge('DEMO_gre_step0.seq');
+% rename:
+% module1.mod => tipdown.mod
+% module3.mod => readout.mod
+% Update modules.txt accordingly
+toppe.playseq(3);
+
 
 return;
 
