819da5fe7ad7a2b44d48eb162805bf18678ab113
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-09-22 16:57:00 -0400

    Moved variables out of toppe sub-struct On branch dev-v4 	modified:   systemspecs.m

--- +toppe/systemspecs.m
+++ +toppe/systemspecs.m
@@ -19,24 +19,29 @@ function sys = systemspecs(varargin)
 %   maxView         Max view index in Pfile data storage. Also not sure here. Default: 500
 %   maxEcho         Max echo index in Pfile data storage. Default: 16. 
 %   addDelays       Set toppe.<start_core*/myrfdel/daqdel/timetrwait/timessi> = 0 (e.g., for converting to/from Pulseq)
-%   toppe           struct describing the TOPPE interpreter/driver
-%                      toppe.version          Default: 'v2'
-%                      toppe.start_core_rf    Minimum start time (us) for rf modules. Default: 0. 
-%                      toppe.start_core_daq   Minimum start time (us) for data acquisition modules. Default: 126
-%                      toppe.start_core_grad  Minimum start time (us) for gradient-only modules. Default: 0.
-%                      toppe.myrfdel          rf/gradient delay (us). Set to 'psd_rf_wait'.
-%                      toppe.daqdel           daq/gradient delay (us). Set to 'psd_grd_wait'.
-%                      toppe.timetrwait     Required delay at end of module (us). Determined empiricially. Default: 64.
-%                      toppe.timessi        EPIC 'ssi' time, i.e., minimum duration/delay between modules (us). Default: 100.
-%                      toppe.nMaxModules    max number of .mod files. Not known. Default: 30.
-%                      toppe.nMaxWaveforms  max number of columns in waveform array. Not known. Default: 200.
+%   version         Default: 'v4'
 %   gradient        Currently suppports 'xrmw', 'xrm', 'whole', 'zoom'. See toppe.pns(). Default: 'xrm'.
+%   start_core_rf   Minimum start time (us) for rf modules. Default: 0. 
+%   start_core_daq  Minimum start time (us) for data acquisition modules. Default: 126
+%   start_core_grad Minimum start time (us) for gradient-only modules. Default: 0.
+%   myrfdel         rf/gradient delay (us). Set to 'psd_rf_wait'.
+%   daqdel          adc/gradient delay (us). Set to 'psd_grd_wait'.
+%   timetrwait      Required delay at end of module (us). Determined empiricially. Default: 64.
+%   timessi         EPIC 'ssi' time, i.e., minimum duration/delay between modules (us). Default: 100.
+%   nMaxModules     max number of .mod files. Not known. Default: 30.
+%   nMaxWaveforms   max number of columns in waveform array. Not known. Default: 200.
 %
-% Usage examples:
-%  >> sys = systemspecs();                   % use default values
-%  >> sys = system('maxSlice', 50);          % sets maxSlice to 50; otherwise contains default values
+% Typical usage:
+%  >> sys = systemspecs();         % use default values (this is probably rarely what you want)
+%  >> sys = toppe.systemspecs('maxSlew', 12.3, 'slewUnit', 'Gauss/cm/ms', ...
+%    'maxGrad', 5, 'gradUnit', 'Gauss/cm', ...
+%    'myrfdel', 78, ...    % psd_rf_wait (RF/gradient delay)
+%    'daqdel', 84);        % psd_grd_wait (ADC/gradient delay)
+
 
 %% Defaults
+
+% Hardware/design specs
 maxRFDefault = 0.25;
 sys.maxGrad  = 4;
 sys.gradUnit = 'Gauss/cm';
@@ -51,18 +56,18 @@ sys.maxView  = 600;            % also not sure here
 sys.maxEcho  = 16;             % about right
 sys.addDelays = true ;         % False: set time gaps to zero.
 
-% sys.toppe struct relates to the TOPPE interpreter/driver.
-% You probably shouldn't edit these.
-sys.toppe.version = 'v4';
-sys.toppe.start_core_rf  = 0;      % minimum start time (us) for rf modules
-sys.toppe.start_core_daq = 126;    % minimum start time (us) for data acquisition modules
-sys.toppe.start_core_grad = 0;     % minimum start time (us) for gradient-only modules
-sys.toppe.myrfdel    = 78;         % rf/gradient delay (us) ( = psd_rf_wait). Inside scanner: 78. Outside scanner: 94.
-sys.toppe.daqdel     = 84;         % daq/gradient delay (us) (= psd_grd_wait). Inside scanner: 84. Outside scanner: 100.
-sys.toppe.timetrwait = 64;         % required delay at end of module (us)
-sys.toppe.timessi    = 100;        % EPIC 'ssi' time, i.e., minimum duration/delay between modules
-sys.toppe.nMaxModules   = 20;      % Not known at the moment, probably limited by total scanner system memory.
-sys.toppe.nMaxWaveforms = 200;     % Not known at the moment.
+% TOPPE version
+sys.version = 'v4';
+
+% timing
+sys.start_core_rf  = 0;      % minimum start time (us) for rf modules
+sys.start_core_daq = 126;    % minimum start time (us) for data acquisition modules
+sys.start_core_grad = 0;     % minimum start time (us) for gradient-only modules
+sys.myrfdel    = 78;         % rf/gradient delay (us) ( = psd_rf_wait). Inside scanner: 78. Outside scanner: 94.
+sys.daqdel     = 84;         % daq/gradient delay (us) (= psd_grd_wait). Inside scanner: 84. Outside scanner: 100.
+sys.timetrwait = 64;         % required delay at end of module (us)
+sys.timessi    = 100;        % EPIC 'ssi' time, i.e., minimum duration/delay between modules
+sys.tminwait   = 12;         % minimum duration of wait pulse in EPIC code
 
 % gradient subsystem. See also pns.m.
 % Scanner  Gradient coil   chronaxie rheobase alpha  gmax  smax
@@ -74,7 +79,11 @@ sys.toppe.nMaxWaveforms = 200;     % Not known at the moment.
 % values on scanner from /w/config/Scandbdt.cfg + GRSubsystemHWO.xml
 sys.gradient = 'xrm';
 
-%% Substitute specified system values as appropriate
+% other
+sys.nMaxModules   = 20;      % Not known at the moment, probably limited by total scanner system memory.
+sys.nMaxWaveforms = 200;     % Not known at the moment.
+
+%% Substitute specified system values
 sys = toppe.utils.vararg_pair(sys, varargin);
 
 if sys.maxRF ~= maxRFDefault
@@ -83,13 +92,13 @@ end
 
 % If requested, set GE/EPIC-related time gaps to zero
 if ~sys.addDelays
-	sys.toppe.start_core_rf = 0;
-	sys.toppe.start_core_daq = 0;
-	sys.toppe.start_core_grad = 0;
-	sys.toppe.myrfdel    = 0;
-	sys.toppe.daqdel     = 0;
-	sys.toppe.timetrwait = 0;
-	sys.toppe.timessi    = 0;
+	sys.start_core_rf = 0;
+	sys.start_core_daq = 0;
+	sys.start_core_grad = 0;
+	sys.myrfdel    = 0;
+	sys.daqdel     = 0;
+	sys.timetrwait = 0;
+	sys.timessi    = 0;
 end
 
 return
