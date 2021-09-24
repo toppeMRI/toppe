b40a33867d7aae6d76efc978234a8ecfe2af3a19
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-09-24 18:51:14 -0400

    Ready for testing on scanner. On branch dev-v4 	modified:   gre.m

--- examples/3DGRE/gre.m
+++ examples/3DGRE/gre.m
@@ -1,96 +1,90 @@
 function main
 % 3D SPGR sequence example
-%
-% To use the +toppe Matlab package, include the (root) folder containing the +toppe folder in your Matlab path.
 
-%% Set system limits
-% Good to back off a bit on slew to avoid PNS (even if scanner can slew at higher rate).
-% Otherwise, accept default values.
-sys = toppe.systemspecs('maxSlew', 10, 'maxGrad', 4);  
+% Set hardware limits (for design)
+% 'maxSlew' and 'maxGrad' options can be < scanner limit, and can vary across .mod files. 
+sys = toppe.systemspecs('maxSlew', 12.3, 'slewUnit', 'Gauss/cm/ms', ...
+    'maxGrad', 5, 'gradUnit', 'Gauss/cm');
 
-%% Acquisition parameters
+
+% Acquisition parameters
 % fov and voxel size must be square (in-plane)
 matrix = [240 240 10];
 fov  = [24 24 10];       % cm
-flip = 10;               % excitation flip angle (degrees)
+flip = 8;               % excitation flip angle (degrees)
 ncyclesspoil = 2;        % number of cycles of spoiler phase across voxel dimension (applied along x and z)
 
 if matrix(1) ~= matrix(2) | fov(1) ~= fov(2)
-	error('In-plane fov and voxel size must be square');
+    error('In-plane fov and voxel size must be square');
 end
 
-%% Make .mod file containing z spoiler gradient and RF excitation
-ofname = 'tipdown.mod';     % Output file name
+% Make .mod file containing z spoiler gradient and RF excitation
 dur = 2;                    % RF pulse duration (msec)
 slthick = fov(3)*0.8;       % Slab thickness (cm). A bit smaller than fov(3) to avoid aliasing.
 ftype = 'min';              % minimum-phase SLR pulse (good for 3D imaging)
 tbw = 8;                    % time-bandwidth product of SLR pulse 
-toppe.utils.rf.makeslr(flip, slthick, tbw, dur, ncyclesspoil*matrix(3), ...
-                       'ftype', ftype, 'ofname', ofname, 'system', sys);
-%toppe.plotmod(ofname);
+toppe.utils.rf.makeslr(flip, slthick, tbw, dur, ncyclesspoil*matrix(3), sys, ...
+                       'ftype', ftype, 'ofname', 'tipdown.mod');
 
-%% Create readout.mod
-ofname = 'readout.mod';     % output file name
-toppe.utils.makegre(fov(1), matrix(1), fov(3)/matrix(3), ... 
-                    'system', sys, 'ofname', ofname, 'ncycles', ncyclesspoil); 
-%toppe.plotmod(ofname);
+% Create readout.mod
+toppe.utils.makegre(fov(1), matrix(1), fov(3)/matrix(3), sys, ... 
+                    'ofname', 'readout.mod', 'ncycles', ncyclesspoil); 
 
-%% Create scanloop.txt
-rfmod = 1;           % module index, i.e., line number in modules.txt
-readoutmod = 2;
+% Write modules.txt
+modFileText = ['' ...
+'Total number of unique cores\n' ...
+'2\n' ...
+'fname  duration(us)    hasRF?  hasDAQ?\n' ...
+'tipdown.mod\t0\t1\t0\n' ...     % Entries are tab-separated   
+'readout.mod\t0\t0\t1\n' ];
+fid = fopen('modules.txt', 'wt');
+fprintf(fid, modFileText);
+fclose(fid);
+
+% Write scanloop.txt
 rfphs = 0;              % radians
 rf_spoil_seed_cnt = 0;
 rf_spoil_seed = 117;    % degrees
-ii = 1;                 % counts number of module executions
 ny = matrix(2);
 nz = matrix(3);
 
-dabon = 1;
-daboff = 0;
-dabmode = dabon;   % best to leave acquisition on even during disdaqs so auto-prescan gets a signal (?)
-waveform = 1;
-textra = 0;        % add delay at end of module (int, microseconds)
+toppe.write2loop('setup', sys, 'version', 4);  % initialize file
 
-for iz = 0:nz           % We'll use iz=0 for approach to steady-state
-	for iy = 1:ny
+for iz = 0:nz     % We'll use iz=0 for approach to steady-state
+    for iy = 1:ny
+        a_gy = -((iy-1+0.5)-ny/2)/(ny/2);  % negative so it starts at -kymax/-kzmax (convention)
+        a_gz = -((max(iz,1)-1+0.5)-nz/2)/(nz/2);
 
-		% rf excitation block (usage of 'block' here parallels its usage in Pulseq)
-		block = [];
-		block.module = rfmod;
-		block.rfscale = 1.0;
-		block.gxscale = 0;
-		block.gyscale = 0;
-		block.gzscale = 1.0;
-		block.rfphs = angle(exp(1i*rfphs));   % radians
-		d(ii,:) = toppe.blockstruct2vec(block);
-		ii = ii + 1;
+        toppe.write2loop('tipdown.mod', sys, ...
+            'RFphase', rfphs);
 
-		% readout
-		block = [];
-		block.module =  readoutmod;
-		block.rfscale = 1.0;
-		block.gxscale = 1.0;
-		block.gyscale = ((iy-1+0.5)-ny/2)/(ny/2);    % phase-encode amplitude scaling, range is [-1 1]
-		block.gzscale = ((iz-1+0.5)-nz/2)/(nz/2);    % partition-encode amplitude scaling
-		block.dabslice = max(iz,0);                  % Convention: skip dabslice=0 
-		block.dabecho = 0; 
-		block.dabview = iy;                          % Convention: skip baseline (0) view
-		block.recphs = angle(exp(1i*rfphs));         % radians
-		d(ii,:) = toppe.blockstruct2vec(block);
-		ii = ii + 1;
+        toppe.write2loop('readout.mod', sys, ...
+            'Gamplitude', [1.0 a_gy a_gz]', ...
+            'DAQphase', rfphs, ...
+            'textra', 0, ...  % slow down scan a bit (ms)
+            'slice', max(iz,1), 'view', iy);
 
-		% update rf/rec phase
-		rfphs = rfphs + (rf_spoil_seed/180 * pi)*rf_spoil_seed_cnt ;  % radians
-		rf_spoil_seed_cnt = rf_spoil_seed_cnt + 1;
-	end
+        % update rf/rec phase
+        rfphs = rfphs + (rf_spoil_seed/180*pi)*rf_spoil_seed_cnt ;  % radians
+        rf_spoil_seed_cnt = rf_spoil_seed_cnt + 1;
+    end
 end
 
-% write loop array to scanloop.txt
-toppe.loop2txt(d);
+% finalize scanloop.txt
+toppe.write2loop('finish', sys);
 
-%% Play sequence in loop (movie) mode
-nModulesPerTR = 2;
-toppe.playseq(nModulesPerTR, 'nTRskip', 10);
+% Play sequence in loop (movie) mode
+%nModulesPerTR = 2;
+%toppe.playseq(nModulesPerTR, 'nTRskip', 10);
+
+% Create 'sequence stamp' file for TOPPE.
+% This file is listed in the 5th row in toppe0.meta
+toppe.preflightcheck('toppe0.meta', 'seqstamp.txt', sys);
+
+% Write files to tar archive (for convenience only).
+% toppe0.meta must be placed in /usr/g/research/pulseq/ on scanner host.
+% The other files go in the folder specified on the first line in toppe0.meta.
+system('tar cf gre.tar toppe0.meta modules.txt scanloop.txt *.mod seqstamp.txt');
 
 return;
 
