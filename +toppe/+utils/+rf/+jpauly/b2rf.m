8d307324afee7b632bb01e5a49af9a3c41315620
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-10-04 13:59:17 -0400

    So mex files are needed On branch main 	new file:   ab2rf.m 	new file:   b2a.m 	new file:   b2rf.m

---
+++ +toppe/+utils/+rf/+jpauly/b2rf.m
@@ -0,0 +1,25 @@
+function rf = b2rf(bc);
+% function rf = b2rf(bc);
+%
+% This function takes a beta polynomial and returns an RF pulse
+% A minimum phase alpha polynomial is intermediately computed,
+% followed by the SLR transform.
+% 
+% Works for complex RF pulses
+% Requires b2a.m and ab2rf.m
+%
+% Inputs:
+%   bc - beta polynomial coefficients
+%
+% Outputs:
+%   rf - RF pulse
+%
+% Original Code from John Pauly's rf_tools package
+% Modified (slightly) by Peder Larson, 12/13/2005
+% (c) Board of Trustees, Leland Stanford Junior University
+
+import toppe.utils.rf.jpauly.*
+
+% Compute minimum phase alpha
+ac = b2a(bc);
+rf = ab2rf(ac, bc);
