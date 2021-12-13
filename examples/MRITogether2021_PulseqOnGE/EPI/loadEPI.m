a426252ab3583620c7320c38bcaf30e52fa91fab
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-12-13 13:03:50 -0500

    On branch develop 	modified:   loadEPI.m

--- examples/MRITogether2021_PulseqOnGE/EPI/loadEPI.m
+++ examples/MRITogether2021_PulseqOnGE/EPI/loadEPI.m
@@ -1,4 +1,4 @@
-function d = loadexchangedata(pfile, readout, din)
+function [d, ims] = loadEPI(pfile, readout, din)
 % Run this from the location of the Pfile, e.g.,
 % /mnt/storage/jfnielse/exchange/...
 
@@ -12,11 +12,7 @@ din = flipdim(din, 1); % !! Data seems to always be flipped along readout (do ch
 %din = permute(d, [1 5 4 2 3]);  % [ndat nshots nz ncoils nscans]
 end
 
-[ndat ncoils ndabslices ndabechoes ndabviews] = size(din);
-
-nscans = ndabslices;
-nz = ndabechoes;
-nshots = ndabviews;
+[ndat ncoils nz nscans nshots] = size(din);
 
 % get readout waveform
 % see makeepi.m
@@ -28,58 +24,29 @@ Ry = paramsint16(4);
 nSampPerEcho = paramsint16(5) + paramsint16(6);
 nramp = paramsint16(7);
 
-etl = ny/nshots/Ry;  % echo train length
+% echo train length
+etl = ny/nshots/Ry;  
 
-% sort data into [ny ny nz ncoils nscans] matrix
+% sort data into [nx ny nz ncoils nscans] matrix
 % size(din) = [ndat ncoils nscans nz nshots]
 fprintf('Reshaping data matrix...');
-d = zeros(nx, ncoils, nscans, nz, ny);
+d = zeros(nx, ncoils, nz, nscans, ny);
 for ish = 1:nshots
-    for e = 1:etl  % loop over echoes
+    for e = 1:etl  % loop over EPI echoes
         iy = ish + (e-1)*nshots;
         iStart = (e-1)*nSampPerEcho + nramp + 1;
         iStop = iStart + nx - 1;
         d(:, :, :, :, iy) = din(iStart:iStop, :, :, :, ish);   
     end
 end
-d = permute(d, [1 5 4 2 3]);
-fprintf('done\n');
-
-return
-
-% slice to write to .mat file
-sl = 5;   % middle slice
-
-% recon
-fprintf('recon..');
-% recon complex coil SPGR images (for B1+ and B0 estimation)
-
-for s = 1:nscans
-    fprintf('.');
-    [spgr(:,:,:,:,s) tmp] = toppe.utils.ift3(d(:,:,:,:,s));
-end
-fprintf('\t');
-spgr = spgr((end/2-nx/2):(end/2+nx/2-1), :, sl, :, :);
-spgr = squeeze(spgr);  % [nx ny nz ncoils 2]
-
-if scanType == 1
-    save('bs.mat', '-v7.3', 'spgr');
-    return;
-else
-    save(sprintf('spgr,design%d.mat', scanType), '-v7.3', 'spgr');
-end
-
-% recon root-sum-of-squares STFR images
-clear stfr
-for s = 3:size(d,5)
-    fprintf('.');
-    [tmp stfr(:,:,:,s-2)] = toppe.utils.ift3(d(:,:,:,:,s));
-end
-stfr = stfr((end/2-nx/2):(end/2+nx/2-1), :, sl, :);
-stfr = squeeze(stfr);  % [nx ny nz nscans-2]
-im(stfr, [0 0.3*max(stfr(:))]);
+d = permute(d, [1 5 3 2 4]);   % [nx ny nz ncoils nscans]
 fprintf(' done\n');
 
-save(sprintf('stfr,design%d.mat', scanType), '-v7.3', 'stfr');
+% Reconstruct
+ims = zeros(nx, ny, nz, nscans);
+for isc = 1:nscans
+    [~, ims(:,:,:,isc)] = toppe.utils.ift3(d(:,:,:,:,isc));
+end
 
 return
+
