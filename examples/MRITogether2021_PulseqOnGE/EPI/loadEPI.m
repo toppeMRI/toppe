d78bdaa339143f4a0f264c7a784ec36adc49c2e8
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-12-13 13:14:54 -0500

    On branch develop 	modified:   loadEPI.m

--- examples/MRITogether2021_PulseqOnGE/EPI/loadEPI.m
+++ examples/MRITogether2021_PulseqOnGE/EPI/loadEPI.m
@@ -43,10 +43,34 @@ d = permute(d, [1 5 3 2 4]);   % [nx ny nz ncoils nscans]
 fprintf(' done\n');
 
 % Reconstruct
-ims = zeros(nx, ny, nz, nscans);
+ims = zeros(nx, ny, nz, nscans);   % root-sum-of-squares coil-combined images
 for isc = 1:nscans
     [~, ims(:,:,:,isc)] = toppe.utils.ift3(d(:,:,:,:,isc));
 end
+figure
+im(ims(:,:,:,1));
+title('3D image volume, scan 1 (flip angle = 5 deg)')
+
+% Extract one slice and display images for all flip angles
+figure
+sl = 6;  % slice
+ims = squeeze(ims(:,:,sl,:));
+im(ims)
+title('Slice 6, flips = 5:5:40')
+
+% Plot signal vs flip angle in an ROI
+figure
+imagesc(ims(:,:,1));
+roi = roipoly;
+for isc = 1:nscans
+    tmp = ims(:,:,isc);
+    s(isc) = mean(tmp(roi));
+end
+flip = 5:5:40;
+plot(flip, s);
+title('Signal vs flip angle');
+xlabel('flip angle (deg)');
+ylabel('SPGR signal');
 
 return
 
