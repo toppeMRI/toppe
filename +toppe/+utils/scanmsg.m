57b4d8222fcb061bc9487878eee334cf898e1677
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-03-26 14:25:33 -0400

    Prints scan instructions to Matlab terminal On branch develop 	new file:   scanmsg.m

---
+++ +toppe/+utils/scanmsg.m
@@ -0,0 +1,16 @@
+function scanmsg(entryFile)
+% function scanmsg()
+%
+% Print scan instructions to terminal
+
+fid = fopen(entryFile, 'r');
+if fid == -1
+    error('Failed to open entry file');
+end
+filePath = fgetl(fid);  % location of TOPPE scan files
+fclose(fid);
+
+fprintf('\nRename the .entry file and copy to /usr/g/research/pulseq/ on scanner host.\n');
+fprintf('  (For example, name it ''toppe10.entry'' and set user CV1 = 10 when prescribing the scan.)\n');
+fprintf(sprintf('Place all other files in %s on scanner host.\n', filePath));
+
