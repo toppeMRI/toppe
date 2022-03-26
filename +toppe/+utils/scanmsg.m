bf801fa483e99d8da8b4e8e857c35f5b7f605339
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-03-26 16:06:36 -0400

    On branch develop 	modified:   scanmsg.m

--- +toppe/+utils/scanmsg.m
+++ +toppe/+utils/scanmsg.m
@@ -10,7 +10,8 @@ end
 filePath = fgetl(fid);  % location of TOPPE scan files
 fclose(fid);
 
-fprintf('\nRename the .entry file and copy to /usr/g/research/pulseq/ on scanner host.\n');
+fprintf('\nRename the .entry file as needed and copy to /usr/g/research/pulseq/ on scanner host.\n');
 fprintf('  (For example, name it ''toppe10.entry'' and set opuser1 (user CV1) = 10 when prescribing the scan.)\n');
+fprintf('Check that the scan file path on line 1 in the .entry file is correct.\n');
 fprintf(sprintf('Place all other files in %s on scanner host.\n', filePath));
 
