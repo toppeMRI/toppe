3ee8409ceca8bc7955be14aa525aff15ed433aa8
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-07-15 14:56:02 -0400

    Return kspace and sample times On branch loadepi 	modified:   loadepi.m

--- +toppe/+utils/loadepi.m
+++ +toppe/+utils/loadepi.m
@@ -1,5 +1,5 @@
-function [d, kx, ky, kz, ti] = loadepi(pfile, echo, readoutFile, varargin)
-% function [d, kx, ky, kz, ti] = loadepi(pfile, echo, readoutFile, varargin)
+function [d, K, T] = loadepi(pfile, echo, readoutFile, varargin)
+% function d = loadepi(pfile, echo, readoutFile, varargin)
 %
 % Load raw data Pfile acquired with scan created by makeepi.m
 %
@@ -17,6 +17,7 @@ function [d, kx, ky, kz, ti] = loadepi(pfile, echo, readoutFile, varargin)
 %        In TOPPE, samples are acquired every 4us.
 %        The 'effective'/design dwell time is decimation*4us, i.e.,
 %        the image needs to be cropped (in x) if decimation > 1.
+
 %   kx/ky   [nx*decimation ny nz]  cycles/cm
 %   ti   [nx*decimation ny nz]  sample times (sec)
 
@@ -57,27 +58,34 @@ ky = interp1(1:nk, ky, (1:nk) - 0.5, 'linear', 'extrap');
 kx = kx - max(kx)/2; % center at k=0 (assume the prephaser does this)
 ky = ky - max(ky)/2;
 kz = zeros([nx*decimation ny nz]);
-dkz = 1/zfov;  % cycles/cm  (kspace spacing)
+dkz = 1/arg.zfov;  % cycles/cm  (kspace spacing)
 for iz = 1:nz
-    kz(:,:,iz) = (iz-nz/2-0.5)*nz*dkz;
+    kz(:,:,iz) = (iz-nz/2-0.5)*dkz;
 end
 
 % sample times
 ti = raster*[[1:nk] - 0.5];  % sec
 
 % sort data into [nx*decimation ny nz nCoils] matrix
-fprintf('Reshaping data matrix... ');
 d = zeros(nx*decimation, nCoils, nz, ny);
+T = zeros(nx*decimation, ny, nz);
 for ish = 1:nShots
     for e = 1:etl  % loop over echoes
         iy = ish + (e-1)*nShots;
         iStart = (e-1)*nSampPerEcho + nRamp + 1;
         iStop = iStart + nx*decimation - 1;
         d(:, :, :, iy) = din(iStart:iStop, :, :, ish);   
+        for iz = 1:nz
+            T(:, iy, iz) = ti(iStart:iStop);      % sample times
+            K(:, iy, iz, 1)  = kx(iStart:iStop);  % kx
+            K(:, iy, iz, 2)  = ky(iStart:iStop);  % ky
+        end
     end
 end
 d = permute(d, [1 4 3 2]);
-fprintf('done\n');
+
+K(:,:,:,3) = kz;
+
 
 return
 
