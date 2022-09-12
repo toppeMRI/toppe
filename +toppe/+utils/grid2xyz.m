cf356c951f2c0b5766d75c870b3ad761b7da434f
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-09-11 22:54:38 -0400

    On branch develop 	renamed:    getroi.m -> +utils/getroi.m 	new file:   +utils/grid2xyz.m 	renamed:    roi2mask.m -> +utils/roi2mask.m

---
+++ +toppe/+utils/grid2xyz.m
@@ -0,0 +1,16 @@
+function [X, Y, Z] = grid2xyz(N, FOV)
+% function [X, Y, Z] = grid2xyz(N, FOV)
+%
+% Return spatial locations consistent with the 
+% 'universal coordinate system' (UCS) that is used by various
+% utitilites such as the Java SlicePlanner and the 
+% B0 shimming tool.
+% This is a right-handed system with cm units:
+% Patient R = +x; A = +y; S = +z; with (0,0,0) at scanner iso-center
+
+FOVc = FOV - FOV./N;   % Distance between edge grid points
+
+[X, Y, Z] = ndgrid(linspace(1,-1,N(1))*FOVc(1)/2, ...
+                   linspace(1,-1,N(2))*FOVc(2)/2, ...
+                   linspace(1,-1,N(3))*FOVc(3)/2);
+
