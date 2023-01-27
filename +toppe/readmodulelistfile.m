e7436cd90a5178cfa6526c9bd8da7fe124e57f2b
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-01-27 13:25:57 -0500

    Added support for trigpos column (v5) On branch develop 	modified:   readmodulelistfile.m

--- +toppe/readmodulelistfile.m
+++ +toppe/readmodulelistfile.m
@@ -19,28 +19,45 @@ if ~exist('modulesListFile', 'var')
 	modulesListFile = 'modules.txt';
 end
 
-% get waveforms
+% read modules.txt and get waveforms
 fid = fopen(modulesListFile, 'r', 'ieee-be');
 s = fgets(fid);  % skip line
 ncores = fscanf(fid, '%d\n', 1);
 s = fgets(fid);  % skip line
 for ic = 1:ncores
+    % .mod file name
 	modArr{ic}.fname = fscanf(fid, '%s\t', 1);
-	modArr{ic}.dur = fscanf(fid, '%d\t', 1);
+
+    % read integer values (only reads one line since it encounters a string on next line)
+    v = fscanf(fid, '%d');
+
+	modArr{ic}.dur = v(1);
     if modArr{ic}.dur < 0
         error('Module duration must be >= 0');
     end
-	modArr{ic}.hasRF = fscanf(fid, '%d\t', 1);
+
+	modArr{ic}.hasRF = v(2);
     if modArr{ic}.hasRF ~= 0 & modArr{ic}.hasRF ~= 1
         error('hasRF must be 0 or 1');
     end
-	modArr{ic}.hasDAQ = fscanf(fid, '%d\n', 1);
+
+    modArr{ic}.hasDAQ = v(3);
+
+    if length(v) > 3
+	    modArr{ic}.trigpos = v(4);
+    else
+	    modArr{ic}.trigpos = -1;
+    end
+
     if modArr{ic}.hasDAQ ~= 0 & modArr{ic}.hasDAQ ~= 1
         error('hasDAQ must be 0 or 1');
     end
+
     if modArr{ic}.hasRF == 1 & modArr{ic}.hasDAQ == 1
         error('Module must be either an RF or DAQ module (or neither)');
     end
+
+    % read .mod file
 	[modArr{ic}.rf, modArr{ic}.gx, modArr{ic}.gy, modArr{ic}.gz, ...
     desc, modArr{ic}.paramsint16, modArr{ic}.paramsfloat, hdr] = toppe.readmod(modArr{ic}.fname,false);
     modArr{ic}.res = hdr.res;
