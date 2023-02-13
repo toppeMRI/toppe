bb50cde91d01f3cd44bb9381dfdc2949ea99141d
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-02-13 12:19:21 -0500

    On branch dev/v6 	modified:   constants.m

--- +toppe/constants.m
+++ +toppe/constants.m
@@ -4,7 +4,7 @@ C.NULL = 0;
 C.TRAP = 1;
 C.ARBITRARY = 2;
 C.ADC = 1;
-C.max_pg_iamp = 2^15-2;  
+C.MAXIAMP = 2^15-2;  
 
 % internal constants used to avoid writing floats to .mod file
 C.rfscale = 1e4;  
