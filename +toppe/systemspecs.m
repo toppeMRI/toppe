32de795d5c94f4f9b7e6e6f46eb3ef8844c50bf1
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2023-06-16 09:47:26 -0400

    Remove warning if maxRF < default (0.25G) On branch dev/v6 	modified:   systemspecs.m

--- +toppe/systemspecs.m
+++ +toppe/systemspecs.m
@@ -122,9 +122,9 @@ sys = rmfield(sys, 'myrfdel');
 sys = rmfield(sys, 'daqdel');
 
 %% Input checks
-if sys.maxRF ~= maxRFDefault
-    warning('Using non-default max RF setting -- b1 scaling may be incorrect!');
-end
+%if sys.maxRF ~= maxRFDefault
+%    warning('Using non-default max RF setting -- b1 scaling may be incorrect!');
+%end
 
 switch sys.gradient
     case 'xrmw', 
@@ -133,6 +133,7 @@ switch sys.gradient
     case 'zoom',
     case 'hrmb',
     case 'hrmw',
+    case 'magnus',
     otherwise, error('Gradient coil (%s) unkown', sys.gradient);
 end
 
