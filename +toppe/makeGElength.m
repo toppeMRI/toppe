e74b42a27766dad7974f1abb5ffdc98278fa5e07
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-06-18 11:29:05 -0400

    On branch dev/v6 	modified:   makeGElength.m

--- +toppe/makeGElength.m
+++ +toppe/makeGElength.m
@@ -1,5 +1,5 @@
 function g = makeGElength(g)
-% If length not divisible by 4, pad with zeroes at end
+% If length not divisible by 2, pad with zeroes at end
 %
 % function g = makeGElength(g)
 %
@@ -7,9 +7,6 @@ function g = makeGElength(g)
 %   g    waveform array [nt ...]
 %
 
-if mod(size(g,1),4)
-	[nrows,ncols] = size(g);
-	g = [g; zeros(4-mod(length(g),4),ncols)];
-end
+[res, npulses] = size(g, 1);
+g = [g; zeros(mod(res, 2), npulses)];
 
-% EOF
