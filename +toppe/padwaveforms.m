4d61c314131de8fcabb03de6bc11a7e0b5cf9556
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2023-06-26 13:45:58 -0400

    On branch dev/v6 	modified:   padwaveforms.m

--- +toppe/padwaveforms.m
+++ +toppe/padwaveforms.m
@@ -1,9 +1,9 @@
 function [rf, gx, gy, gz] = padwaveforms(varargin)
 % function [rf, gx, gy, gz] = padwaveforms(varargin)
 %
-% Pad input waveform(s) with zero, and return
-% arrays rf/gx/gy/gz of common size [ndat npulses],
-% where ndat is multiple of 2.
+% Return arrays rf/gx/gy/gz of common size [ndat npulses],
+% where ndat = max waveform length, and 
+% npulses = max number of waveforms (pulses) per channel/axis
 %
 % Input options:
 %   rf            [nrf nrfpulses] Complex RF waveform, [ndat nrfpulses]
@@ -12,8 +12,7 @@ function [rf, gx, gy, gz] = padwaveforms(varargin)
 %   gz            [ngz ngzpulses]
 %
 % Outputs:
-%   rf            [ndat npulses], where ndat = max waveform length, and 
-%                 npulses = max number of waveforms (pulses) per axis
+%   rf            [ndat npulses]
 %   gx            [ndat npulses]
 %   gy            [ndat npulses]
 %   gz            [ndat npulses]
@@ -31,16 +30,15 @@ arg.gz = [];
 %arg = toppe.utils.vararg_pair(arg, varargin);
 arg = vararg_pair(arg, varargin);
 
-%% Copy input waveform to rf, gx, gy, and gz (so we don't have to carry the arg. prefix around)
+% Copy input waveform to rf, gx, gy, and gz (so we don't have to carry the arg. prefix around)
 fields = {'rf' 'gx' 'gy' 'gz'};
 for ii = 1:length(fields)
-    wavtype = fields{ii} ;   % 'rf', 'gx', 'gy', or 'gz'
-    cmd = sprintf('%s = %s;', wavtype, sprintf('arg.%s', wavtype));
+    channel = fields{ii} ;   % 'rf', 'gx', 'gy', or 'gz'
+    cmd = sprintf('%s = %s;', channel, sprintf('arg.%s', channel));
     eval(cmd);
 end
 
-%% Force all waveform arrays to have the same dimensions.
-%% and make length multiple of 2.
+%% Force all channels to have dimension [ndat npulses]
 ndat    = max( [size(rf,1) size(gx,1) size(gy,1) size(gz,1)] );
 npulses = max( [size(rf,2) size(gx,2) size(gy,2) size(gz,2)] );
 if ndat == 0
@@ -51,35 +49,32 @@ if ndat > 2^15
 end
 
 % make length divisible by 2 (EPIC seems to behave best this way)
-if mod(ndat, 2)
-    warning('Waveform duration will be padded to 2 sample boundary.')
-    ndat = ndat + mod(ndat, 2);
-end
+%if mod(ndat, 2)
+%    warning('Waveform duration will be padded to 2 sample boundary.')
+%    ndat = ndat + mod(ndat, 2);
+%end
 
 for ii = 1:length(fields)
-    wavtype = fields{ii} ;                    % 'rf', 'gx', 'gy', or 'gz'
+    channel = fields{ii} ;                    % 'rf', 'gx', 'gy', or 'gz'
     wav = eval(fields{ii}) ;                  % [ndat npulses]
 
-    if wavtype == 'rf' & isempty(wav)
+    if isempty(wav)
+        wav = zeros(ndat, npulses);
+    end
+
+    if channel == 'rf' & isempty(wav)
         % Must have non-zero RF waveform to make toppe happy (even if it's not actually played out)
-        wav = [zeros(1,npulses); 0.01*ones(ndat-2, npulses); zeros(1,npulses)];
+        wav = [0; 0.01*ones(ndat-2, 1); 0];
     end
 
-    % enforce equal number of rows and columns
-    [nrows n2] = size(wav);
+    [nrows ncols] = size(wav);
+    wavnew = zeros(ndat, npulses);
 
-    if (nrows ~=0 & nrows < ndat) 
-        warning('Padding %s with zero rows', wavtype);
-    end
-    if (n2 ~= 0 & n2 < npulses) 
-        warning('Padding %s with zero columns', wavtype);
+    for ic = 1:ncols
+        wavnew(:,ic) = [wav(:,ic); wav(end,ic)*ones(ndat-nrows, 1)];
     end
 
-    wav = [wav; zeros(ndat-nrows,n2)];
-    wav = [wav  zeros(ndat,npulses-n2)];
-
-    % copy to corresponding wav type (rf, gx, gy, or gz)
-    cmd = sprintf('%s = %s;', wavtype, 'wav') ;
+    cmd = sprintf('%s = wavnew;', channel) ;
     eval (cmd);
 end
     
