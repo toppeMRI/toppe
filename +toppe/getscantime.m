97eb206bf4f63bd26a19f52cc26eb4b52a096cd1
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-08-09 13:50:48 -0400

    Merge changes from develop branch On branch main 	modified:   getscantime.m 	modified:   plotseq.m 	modified:   preflightcheck.m

--- +toppe/getscantime.m
+++ +toppe/getscantime.m
@@ -1,10 +1,12 @@
-function dur = getscantime(system, varargin)
-% Get total scan time for a TOPPE scan.
-%
+function dur = getscantime(sysGE, varargin)
 % function dur = getscantime(system, varargin)
 %
+% Get total scan time for a TOPPE scan.
+% Like the interpreter on the scanner, this function
+% load the sequence files present in the current folder.
+%
 % Input:
-%   system             struct specifying hardware system info, see systemspecs.m
+%   sysGE              struct specifying hardware system info, see systemspecs.m
 %
 % Input options:
 %   loopFile           Default: 'scanloop.txt'
@@ -16,29 +18,6 @@ function dur = getscantime(system, varargin)
 %
 % Output:
 %    dur          sec
-%
-% Examples:
-% >> toppe.getscantime();
-
-% This file is part of the TOPPE development environment for platform-independent MR pulse programming.
-%
-% TOPPE is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Library General Public License as published by
-% the Free Software Foundation version 2.0 of the License.
-%
-% TOPPE is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Library General Public License for more details.
-%
-% You should have received a copy of the GNU Library General Public License
-% along with TOPPE. If not, see <http://www.gnu.org/licenses/old-licenses/lgpl-2.0.html>.
-%
-% (c) 2018 The Regents of the University of Michigan
-% Jon-Fredrik Nielsen, jfnielse@umich.edu
-%
-% $Id: getscantime.m,v 1.4 2018/10/25 12:40:29 jfnielse Exp $
-% $Source: /export/home/jfnielse/Private/cvs/projects/psd/toppe/matlab/+toppe/getscantime.m,v $
 
 import toppe.*
 import toppe.utils.*
@@ -67,23 +46,7 @@ else
    mods = arg.mods;
 end
 
-%% Call plotseq with 'doTimeOnly' argument to get the length of rho quickly
-% Do it in groups of rows (in scanloop.txt) to reduce memory requirements
-dt = 4e-6;    % duration of one gradient/rf sample (sec)
-dur = 0;
-nl = size(loopArr,1);   % number of rows in scanloop.txt (startseq() calls)
-nlPerGroup = 1e3;
-if nl > 2*nlPerGroup
-	for ii = 1:nlPerGroup:(nl-nlPerGroup)   %size(loopArr,1)
-		rho = toppe.plotseq(ii, ii+nlPerGroup-1, system, 'loopArr', loopArr, 'mods', mods, 'doTimeOnly', true);
-		dur = dur + size(rho,1)*dt;
-	end
-	nlLeft = max(0, nl - (ii+nlPerGroup-1));
-	rho = toppe.plotseq(ii+1, ii+nlLeft-1, system, 'loopArr', loopArr, 'mods', mods, 'doTimeOnly', true);
-	dur = dur + size(rho,1)*dt;
-else
-	rho = toppe.plotseq(1, size(loopArr,1), system, 'loopArr', loopArr, 'mods', mods, 'doTimeOnly', true);
-	dur = dur + size(rho,1)*dt;
-end
-%dur = round(dur);
-%fprintf('Total scan time: %dm %ds\n', floor(dur/60), dur - 60*floor(dur/60) );
+%% Call plotseq with 'doTimeOnly' argument to get the sequence duration quickly
+[rf,gx,gy,gz,T] = toppe.plotseq(sysGE, 'timeRange', [0 inf], 'doTimeOnly', true);
+dur = T(2);
+%fprintf('Total scan time: %dm %0.1f s\n', floor(dur/60), dur - 60*floor(dur/60) );
--- +toppe/plotseq.m
+++ +toppe/plotseq.m
@@ -1,51 +1,79 @@
-function [rf, gx, gy, gz] = plotseq(nstart, nstop, sysGE, varargin)
-% function [rf, gx, gy, gz] = plotseq(nstart, nstop, sysGE, varargin)
+function [rf, gx, gy, gz, tRange] = plotseq(sysGE, varargin)
+% function [rf, gx, gy, gz, tRange] = plotseq(sysGE, varargin)
 %
-% Display pulse sequence, as specified in modules.txt, scanloop.txt, and timing.txt.
+% Display pulse sequence, as specified in modules.txt and scanloop.txt in current folder.
 %
-% Sequence timing here is approximate.
+% Can also be called the old way for backward compatibility:
+%     function plotseq(nStart, nStop, sysGE, varargin)
+%
+% NB! Sequence timing is approximate (for now).
 % See v6/figs/timing.svg in the TOPPEpsdSourceCode Github repo for detailed sequence timing.
 %
 % Inputs:
-%   nstart,nstop       first and last startseq calls (as specified in scanloop.txt)
 %   sysGE             struct specifying hardware system info, see systemspecs.m
+%
+% Options:
+%   'timeRange'       Specify time range, [tStart tStop] (sec)
+%   'blockRange'      Block range, [iStart iStop]
+%
+% Outputs:
+%   tRange            [tStart tStop]  Start and stop times (sec)
+%
+% Example:
+%  Plot the whole sequence:
+%    >> toppe.plotseq(sysGE, 'timeRange', [0 inf]);
+%    or just:
+%    >> toppe.plotseq(sysGE);
 
-%% parse inputs
-arg.loopArr         = [];
+
+%% parse input options
+
+% Support the old way of calling plotseq: plotseq(nStart, nStep, sysGE, varargin)
+if isnumeric(sysGE)
+    arg.blockRange = [sysGE varargin{1}];
+    sysGE = varargin{2};
+    if length(varargin) > 2
+        varargin = varargin(3:end);
+    else
+        varargin = {};
+    end
+else
+    arg.blockRange = [];
+end
+
+% defaults
+arg.timeRange       = [];
+arg.loop            = [];
 arg.loopFile        = 'scanloop.txt';
 arg.mods            = [];
 arg.moduleListFile  = 'modules.txt';
 arg.doDisplay       = true;
 arg.doTimeOnly      = false;
-arg.drawpause       = 1;
-arg.gmax            = [];     % Gauss/cm
+arg.gmax            = [];  % Gauss/cm
 arg.rhomax          = [];  % Gauss
 arg.printTime       = false;
 
 arg = toppe.utils.vararg_pair(arg, varargin);
 
-if isempty(arg.gmax)
-    arg.gmax = sysGE.maxGrad;  % default
-end
-if isempty(arg.rhomax)
-    arg.rhomax = sysGE.maxRF;  % default
+if isempty(arg.timeRange) & isempty(arg.blockRange)
+    arg.timeRange = [0 inf];
 end
 
-%% read scan files as needed
-% scanloop array
-if isempty(arg.loopArr)
-    loopArr = toppe.tryread(@toppe.readloop, arg.loopFile);
+arg.timeRange = round(arg.timeRange*1e6);   % us
+
+if arg.doTimeOnly
+    arg.doDisplay = false;
+end
+
+%% read scan files
+% scanloop 
+if isempty(arg.loop)
+    loop = toppe.tryread(@toppe.readloop, arg.loopFile);
 else
-    loopArr = arg.loopArr;
+    loop = arg.loop;
 end
 
-if size(loopArr,2) > 27
-    toppeVersion = 6;
-else
-    toppeVersion = 5;
-end
-
-% module waveforms
+% modules
 if isempty(arg.mods)
     modules = toppe.tryread(@toppe.readmodulelistfile, arg.moduleListFile);
 else
@@ -61,123 +89,172 @@ if arg.doTimeOnly
     end
 end
 
-%% Get block groups
-if toppeVersion > 5
-    blockGroups = toppe.readcoresfile('cores.txt');
-end
+%% Get block groups. Not used at the moment [TODO]
+%blockGroups = toppe.readcoresfile('cores.txt');
+
+%if arg.doTimeOnly 
+%    gxlength = round(modules{p}.dur/raster);
+%    nsamples = nsamples + gxlength;
 
 %% build sequence. each sample is 4us.
 rho = []; th = []; gx = []; gy = []; gz = [];
-dt = 4;  % us
-max_pg_iamp = 2^15-2;
 raster = sysGE.raster;  % us
 
-for n = nstart:nstop
-
-    p = loopArr(n,1);   % module id
-    if toppeVersion > 5
-        i = loopArr(n,28);  % block group id
-    else
-        i = p;
-    end
-    w = loopArr(n,16);  % waveform index
-
-    % Pure delay block
-    if p == 0
-       w = zeros(round(loopArr(n,14)/raster), 1);  % TODO: make accurate
-       rho = [rho; w];
-       th = [th; w];
-       gx = [gx; w];
-       gy = [gy; w];
-       gz = [gz; w];
-       continue;
-    end
-
-    if modules{p}.hasRF
-        ia_rf = loopArr(n,2);
-    else
-        ia_rf = 0;
-    end
-    ia_th = loopArr(n,3);
-    ia_gx = loopArr(n,4);
-    ia_gy = loopArr(n,5);
-    ia_gz = loopArr(n,6);
-
-    if arg.doTimeOnly % Calculate the length of one waveform and add it to our sample counter
-        gxlength = round(modules{p}.dur/raster);
-        nsamples = nsamples + gxlength;
-    else % Calculate RF and gradients as normal
-        % waveforms for this block
-        rho1 = [ia_rf/max_pg_iamp*abs(modules{p}.rf(:,w))];
-        th1 = [ia_rf/max_pg_iamp*angle(modules{p}.rf(:,w))];
-        gxit = ia_gx/max_pg_iamp*modules{p}.gx(:,w);
-        gyit = ia_gy/max_pg_iamp*modules{p}.gy(:,w);
-        gzit = ia_gz/max_pg_iamp*modules{p}.gz(:,w);
-
-        % apply 3d rotation matrix 
-        % (which also accounts for any in-plane 2D rotation, i.e., 'phi' in write2loop.m)
-        Rv = loopArr(n,17:25)/max_pg_iamp;  % stored in row-major order
-        R = reshape(Rv, 3, 3);
-        G = R * [gxit(:)'; gyit(:)'; gzit(:)'];
-        gx1 = G(1,:)';
-        gy1 = G(2,:)';
-        gz1 = G(3,:)';
-
-        % apply RF phase offset
-        if modules{p}.hasRF
-            th1 = th1 + loopArr(n,12)/max_pg_iamp*pi;
-            th1 = angle(exp(1i*th1));   % wrap to [-pi pi] range
+if ~isempty(arg.blockRange)
+    % get start and stop times (for plotting)
+    n = 1;
+    t = 0;
+    for n = 1:(arg.blockRange(1)-1)
+        p = loop(n,1);   % module id
+        if p == 0
+            t = t + loop(n,14);    % us
+        else
+            t = t + modules{p}.dur;
         end
-
-        % pad to desired duration and add to running waveform
-        res = round(modules{p}.dur/raster);
-        rho = [rho; rho1; zeros(res - modules{p}.res, 1)];
-        th = [th; th1; zeros(res - modules{p}.res, 1)];
-        gx = [gx; gx1; zeros(res - modules{p}.res, 1)];
-        gy = [gy; gy1; zeros(res - modules{p}.res, 1)];
-        gz = [gz; gz1; zeros(res - modules{p}.res, 1)];
     end
-    
-    if arg.printTime
-        fprintf(1, 'n %d: mindur = %d us, rf t = %d us, grad t = %d us\n', n, mindur, numel(rho)*raster, numel(gx)*raster);
+    tStart = t;
+    for n = arg.blockRange(1):arg.blockRange(2)
+        p = loop(n,1);   % module id
+        if p == 0
+            t = t + loop(n,14);    % us
+        else
+            t = t + modules{p}.dur;
+        end
     end
-end
+    tStop = t;
+    T = (tStart+raster/2) : raster : tStop;
 
-if arg.doTimeOnly % Make all vectors the correct length but zeros
-    [rf, th, gx, gy, gz] = deal(zeros(nsamples,1));
+    tRange = [tStart tStop]*1e-6;  % s
+
+    [rho, th, gx, gy, gz, dur] = sub_getwavs(arg.blockRange(1), arg.blockRange(2), loop, modules, sysGE, arg.doTimeOnly);
 else
-    rf = rho.*exp(1i*th);
+    % Plot time range
+    tic = arg.timeRange(1);
+    toc = arg.timeRange(2);
+
+    % find blocks containing start and stop times
+    n = 1;
+    t = 0;  % time counter (us)
+    dur = [];
+    while t <= tic 
+        if n == size(loop,1)-1
+            error('invalid start time');
+        end
+        
+        p = loop(n,1);   % module id
+        i = loop(n,28);  % block group id
+
+        if p == 0
+            dur = loop(n,14);    % us
+        else
+            dur = modules{p}.dur;  % us
+        end
+        t = t + dur;
+        n = n + 1;
+    end
+
+    if isempty(dur)
+        error('invalid time range');
+    end
+
+    tStart = max(t-dur, 0);
+    nStart = max(n-1, 1);
+
+    while t < toc & n <= size(loop, 1)
+        p = loop(n,1);   % module id
+        i = loop(n,28);  % block group id
+
+        if p == 0
+            t = t + loop(n,14);    % us
+        else
+            t = t + modules{p}.dur;
+        end
+        n = n + 1;
+    end
+
+    tStop = t;
+    nStop = max(n-1, 1);
+
+    % get waveforms
+    [rho, th, gx, gy, gz, dur] = sub_getwavs(nStart, nStop, loop, modules, sysGE, arg.doTimeOnly);
+
+    if ~arg.doTimeOnly
+        % vector of time points (for plotting)
+        % keep only desired time range
+        T = (tStart+raster/2):raster:tStop;
+        mask = T >= tic & T <= toc;
+        T = T (mask);
+        rho = rho(mask);
+        th = th(mask);
+        gx = gx(mask);
+        gy = gy(mask);
+        gz = gz(mask);
+        tRange = [T(1) T(end)]*1e-6;
+    else
+        tRange = [tStart tStop]*1e-6;
+    end
+
 end
 
-% plot
+rf = rho.*exp(1i*th);
+
+%if arg.printTime
+%    fprintf(1, 'n %d: mindur = %d us, rf t = %d us, grad t = %d us\n', n, mindur, numel(rho)*raster, numel(gx)*raster);
+%end
+
+%% plot
 if arg.doDisplay
-    T = (0:(numel(rho)-1))*raster/1000; % msec
-    if ~arg.drawpause
-        Tend = 1.01*T(find(any([rho th gx gy gz],2),1,'last')); %Find last non-zero value in any of the waveforms
+%    T = (0:(numel(rho)-1))*raster/1000; % msec
+    T = T*1e-3;  % ms
+    
+    Tend = max(T);
+
+    if isempty(arg.gmax)
+        gxmax = 1.3*max(abs(gx));
+        if gxmax == 0
+            gxmax = sysGE.maxGrad;
+        end
+        gymax = 1.3*max(abs(gy));
+        if gymax == 0
+            gymax = sysGE.maxGrad;
+        end
+        gzmax = 1.3*max(abs(gz));
+        if gzmax == 0
+            gzmax = sysGE.maxGrad;
+        end
     else
-        Tend = 1.01*T(end);
+        gxmax = arg.gmax;
+        gymax = arg.gmax;
+        gzmax = arg.gmax;
+    end
+
+    if isempty(arg.rhomax)
+        rhomax = 1.3*max(abs(rho));
+        if rhomax == 0
+            rhomax = sysGE.maxRF;
+        end
+    else
+        rhomax = arg.rhomax;
     end
     
-    gmax = 1.1*arg.gmax;   % Gauss/cm
-    srho = 1.1*arg.rhomax; %max(1.1*max(abs(rho(:))),0.05);
     lw = 2;
     bgColor = 'k';
 
     t = tiledlayout(5, 1);
     ax1 = nexttile;
-    plot(T, gx, '-y', 'LineWidth', lw);  ylabel('X (G/cm)'); axis([T(1) Tend -1.05*gmax 1.05*gmax]);
+    plot(T, gx, '-y', 'LineWidth', lw);  ylabel('X (G/cm)'); axis([T(1) Tend -gxmax gxmax]);
     set(gca, 'color', bgColor);  set(gca, 'XTick', []);
 
     ax2 = nexttile;
-    plot(T, gy, '-c', 'LineWidth', lw);  ylabel('Y (G/cm)'); axis([T(1) Tend -1.05*gmax 1.05*gmax]);
+    plot(T, gy, '-c', 'LineWidth', lw);  ylabel('Y (G/cm)'); axis([T(1) Tend -gymax gymax]);
     set(gca, 'color', bgColor);  set(gca, 'XTick', []);
     
     ax3 = nexttile;
-    plot(T, gz, '-m', 'LineWidth', lw);  ylabel('Z (G/cm)'); axis([T(1) Tend -1.05*gmax 1.05*gmax]);
+    plot(T, gz, '-m', 'LineWidth', lw);  ylabel('Z (G/cm)'); axis([T(1) Tend -gzmax gzmax]);
     set(gca, 'color', bgColor);  set(gca, 'XTick', []);
 
     ax4 = nexttile;
-    plot(T, rho, '-r', 'LineWidth', lw); ylabel('|b1| (Gauss)'); axis([T(1) Tend -srho srho]);
+    plot(T, rho, '-r', 'LineWidth', lw); ylabel('|b1| (Gauss)'); axis([T(1) Tend -rhomax rhomax]);
     set(gca, 'color', bgColor);  set(gca, 'XTick', []);
 
     ax5 = nexttile;
@@ -191,6 +268,73 @@ if arg.doDisplay
     linkaxes([ax1 ax2 ax3 ax4 ax5], 'x');
 end
 
-return;
 
-% EOF
+%% Function to build sequence. Each sample is 4us.
+function [rho, th, gx, gy, gz, dur] = sub_getwavs(blockStart, blockStop, loop, modules, sysGE, doTimeOnly)
+
+rho = []; th = []; gx = []; gy = []; gz = [];
+max_pg_iamp = 2^15-2;
+raster = sysGE.raster;  % us
+
+for n = blockStart : blockStop
+
+    p = loop(n,1);   % module id
+    i = loop(n,28);  % block group id
+    w = loop(n,16);  % waveform index
+
+    dur = 0;
+
+    % Pure delay block
+    if p == 0
+        dur = dur + loop(n,14);  % TODO: make accurate
+        if ~doTimeOnly   
+            w = zeros(round(loop(n,14)/raster), 1);  % TODO: make accurate
+            rho = [rho; w];
+            th = [th; w];
+            gx = [gx; w];
+            gy = [gy; w];
+            gz = [gz; w];
+        end
+        continue;
+    end
+
+    if modules{p}.hasRF
+        ia_rf = loop(n,2);
+    else
+        ia_rf = 0;
+    end
+    ia_th = loop(n,3);
+    ia_gx = loop(n,4);
+    ia_gy = loop(n,5);
+    ia_gz = loop(n,6);
+
+    % Calculate RF and gradient waveforms for this block
+    rho1 = [ia_rf/max_pg_iamp*abs(modules{p}.rf(:,w))];
+    th1 = [ia_rf/max_pg_iamp*angle(modules{p}.rf(:,w))];
+    gxit = ia_gx/max_pg_iamp*modules{p}.gx(:,w);
+    gyit = ia_gy/max_pg_iamp*modules{p}.gy(:,w);
+    gzit = ia_gz/max_pg_iamp*modules{p}.gz(:,w);
+
+    % apply 3d rotation matrix 
+    % (which also accounts for any in-plane 2D rotation, i.e., 'phi' in write2loop.m)
+    Rv = loop(n,17:25)/max_pg_iamp;  % stored in row-major order
+    R = reshape(Rv, 3, 3);
+    G = R * [gxit(:)'; gyit(:)'; gzit(:)'];
+    gx1 = G(1,:)';
+    gy1 = G(2,:)';
+    gz1 = G(3,:)';
+
+    % apply RF phase offset
+    if modules{p}.hasRF
+        th1 = th1 + loop(n,12)/max_pg_iamp*pi;
+        th1 = angle(exp(1i*th1));   % wrap to [-pi pi] range
+    end
+
+    % pad to desired duration and add to running waveform
+    res = round(modules{p}.dur/raster);
+    rho = [rho; rho1; zeros(res - modules{p}.res, 1)];
+    th = [th; th1; zeros(res - modules{p}.res, 1)];
+    gx = [gx; gx1; zeros(res - modules{p}.res, 1)];
+    gy = [gy; gy1; zeros(res - modules{p}.res, 1)];
+    gz = [gz; gz1; zeros(res - modules{p}.res, 1)];
+end
